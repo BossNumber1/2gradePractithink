7fbabbf5e1f405823edd327ae67535e6d4698bfd
BossNumber1 <dimaang23@gmail.com>
2022-02-04 15:47:52 +0300

    maded drag and drop for 23 question

--- RESULT/index.js
+++ RESULT/index.js
@@ -1093,6 +1093,44 @@ function drop17(e) {
     orignalElement.id = currentId;
 }
 
+// 23 QUESTION
+
+function drag23(e) {
+    localStorage.setItem("idOrigin23question2class", e.target.id);
+}
+
+function drop23(e) {
+    // получаем имя и id взятого элемента
+    let idOrig = localStorage.getItem("idOrigin23question2class");
+    let nameObjectOrig = idOrig.slice(0, -1);
+
+    // получаем имя и id, на который кладём элемент
+    let currentId = e.target.id;
+    let nameObjectCurrent = currentId.slice(0, -1);
+
+    // получаем объекты
+    let orignalElement = document.getElementById(idOrig);
+    let currentElement = document.getElementById(currentId);
+
+    // меняем картинки местами
+    currentElement.src = "./pictures/23que/" + nameObjectOrig + ".svg";
+    orignalElement.src = "./pictures/23que/" + nameObjectCurrent + ".svg";
+
+    // меняем id местами
+    currentElement.id = idOrig;
+    orignalElement.id = currentId;
+
+    // меняем фон при определённом условии
+    if (nameObjectCurrent === "emptyPlace") {
+        currentElement.parentElement.style.backgroundColor = "white";
+        currentElement.parentElement.style.border = "none";
+    }
+
+    // меняем вид курсора
+    currentElement.style.cursor = "grab";
+    orignalElement.style.cursor = "default";
+}
+
 // 20 QUESTION
 
 // для полей ввода
